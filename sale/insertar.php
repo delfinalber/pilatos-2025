1e81099c09e3d9bb901844dfdf3744d4a3e9ac23
delfinalber <delfin.alber@gmail.com>
2025-09-22 16:07:52 -0500

    sale formulario de envio de pedido 22-09-2025

--- sale/insertar.php
+++ sale/insertar.php
@@ -8,11 +8,12 @@ $telefono = $_POST['telefono'];
 $email = $_POST['email'];
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
+$mensaje = $_POST['mensaje'];
 
 
 
-if ($nombre && $apellido && $edad && $telefono && $email && $usuario && $password ){
-    $sql= "INSERT INTO registro_sale(id_sale, nombre_sale, apellido_sale, edad_sale, telefono_sale, email_sale, usuario_sale, password_sale, date_sale) VALUES (NULL, '$nombre', '$apellido', '$edad', '$telefono', '$email', '$usuario', '$password', NULL)"; 
+if ($nombre && $apellido && $edad && $telefono && $email && $usuario && $password && $mensaje) {
+    $sql= "INSERT INTO registro_sale(id_sale, nombre_sale, apellido_sale, edad_sale, telefono_sale, email_sale, usuario_sale, password_sale, mesaje_sale, date_sale) VALUES (NULL, '$nombre', '$apellido', '$edad', '$telefono', '$email', '$usuario', '$password', '$mensaje', NULL)"; 
       
     mysqli_query($mysqli, $sql);
        
